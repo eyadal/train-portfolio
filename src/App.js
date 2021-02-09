e1cc94cc5ae9a51f863352c43e1e4c1fb40651e7
eyadal <31968847+eyadal@users.noreply.github.com>
2021-02-09 22:59:32 +0100

    created GlobalStyle file

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import {useState} from 'react'
-
-
+import React from 'react'
+import GlobalStyle from './GlobalStyle'
 function App() {
-  const [inputVal, setInputVal] = useState('')
-  const [todos, setTodos] = useState([''])
-
-  const handleChange = (e)=> {
-    setInputVal(e.target.value)
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setTodos([...todos, inputVal])
-    e.target[0].value =''
-  }
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      {/** 
-       * 2. event handler for the submit ( )  */}
-        <input onChange={handleChange} placeholder='Add Todo..' />
-        <button>Add Todo</button>
-    </form> 
-      <ul>
-        {todos.map( (todo, index) => (
-          todo !== '' ?
-          <li key={index}>{todo}</li> : null
-        ))}
-      </ul>
+      <GlobalStyle />
+      
     </>
-  );
+  )
 }
 
-export default App;
+export default App
